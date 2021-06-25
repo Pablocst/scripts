5e9f0d47759839e86344414b0737a551a3e841a6
Pablocst <pablocstredes@gmail.com>
2021-06-25 18:07:03 -0300

    Update swap_text.py

--- swap_text.py
+++ swap_text.py
@@ -1,14 +1,14 @@
 import sys
 
-path = sys.argv[0:]
-old_ip = sys.argv[1:]
-new_ip = sys.argv[2:]
+path = sys.argv[1]
+old_ip = sys.argv[2]
+new_ip = sys.argv[3]
 f = open(path,'r')
 filedata = f.read()
 f.close()
 
 newdata = filedata.replace(old_ip,new_ip)
 
-f = open(fileout,'w')
+f = open('arquivodnsfinal.txt','w')
 f.write(newdata)
 f.close()
