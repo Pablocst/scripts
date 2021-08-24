95a1044e19e441810e66a6c5a381dee4e0712f20
Pablocst <pablocstredes@gmail.com>
2021-08-24 15:12:43 -0300

    Update removereseller.sh

--- removereseller.sh
+++ removereseller.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
 
+if [ $# -ne 1 ]; then
+  echo -e "Invalid parameter, please use: removereseller.sh [RESELLER_ACCOUNT]: \033[0;31m[ERROR]\033[0m"
+  exit;
+else
+  RESELLER=$1
+fi
+
+for i in `grep $RESELLER /etc/trueuserowners | cut -f1 -d":" | grep -v $RESELLER`; do /scripts/removeacct --force; done
