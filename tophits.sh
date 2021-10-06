e7fd83b8b2f206c722313ae66d6f5cd49b28395a
Pablocst <pablocstredes@gmail.com>
2021-10-06 17:40:57 -0300

    Update tophits.sh

--- tophits.sh
+++ tophits.sh
@@ -3,14 +3,18 @@
 current_d=$(LC_TIME=en_US date +"%d\/%b\/%Y")
 
 touch tmptophits2.txt
-for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done 
+for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done > /dev/null 2>&1
+echo "==================="
 echo "Top 5 Hits porta 80"
+echo "==================="
 cat tmptophits2.txt | sort -n | tail -n5
 rm -f tmptophits2.txt && rm -f tmptophits.txt
 
 touch tmptophits2.txt
-for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-ssl_log-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done 
+for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-ssl_log-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done > /dev/null 2>&1
+echo "==================="
 echo "Top 5 Hits porta 443"
+echo "==================="
 cat tmptophits2.txt | sort -n | tail -n5
 rm -f tmptophits2.txt && rm -f tmptophits.txt
 
