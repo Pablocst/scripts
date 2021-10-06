365ea7f90b5420057eb9e18f7534e2f82b6839aa
Pablocst <pablocstredes@gmail.com>
2021-10-06 17:35:52 -0300

    Update tophits.sh

--- tophits.sh
+++ tophits.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-current_d=$(LC_TIME=en_US date +"%d\/%B\/%Y")
-for i in `cat /etc/userdomains | cut -f1 -d":"`; do for j in `grep $i /etc/userdomains | cut -f2 -d":" | tail -n1`; do VALOR=$(grep $i /var/log/apache2/domlogs/$j/* | grep $current_d | wc -l) && echo "$i : $VALOR"; done; done > /root/tmp_hits_file.txt 2>/dev/null
-cat /root/tmp_hits_file.txt | tr -s " " | awk '!/^$/' | tr -d '[:blank:]' | sort -n -k 2,2 -k 1,1 -t ":" | tail -n5
-rm -rf /root/tmp_hits_file.txt
+current_d=$(LC_TIME=en_US date +"%d\/%b\/%Y")
+
+touch tmptophits2.txt
+for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done 
+echo "Top 5 Hits porta 80"
+cat tmptophits2.txt | sort -n | tail -n5
+rm -f tmptophits2.txt && rm -f tmptophits.txt
+
+touch tmptophits2.txt
+for i in `/bin/ls -lh /var/cpanel/users/ | grep -v system |tr -s " " | cut -f9 -d" "`;do for j in `grep $i /etc/userdomains | cut -f1 -d":"`; do zgrep $current_d /home/$i/logs/$j-ssl_log-Oct-2021.gz | wc -l > tmptophits.txt && echo "$j: " >> tmptophits2.txt  && echo -n $(cat tmptophits.txt) >> tmptophits2.txt && echo "" >> tmptophits2.txt $j;done;done 
+echo "Top 5 Hits porta 443"
+cat tmptophits2.txt | sort -n | tail -n5
+rm -f tmptophits2.txt && rm -f tmptophits.txt
+
