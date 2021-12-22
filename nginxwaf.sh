7c376fa05101019300fb4ea3902c3fc1059f1cba
Pablocst <pablocstredes@gmail.com>
2021-12-22 17:29:47 -0300

    Update nginxwaf.sh

--- nginxwaf.sh
+++ nginxwaf.sh
@@ -1,3 +1,17 @@
+
+
 apt-get update -y
 apt-get install wget -y
 ufw disable
+apt-get install nginx -y
+ufw allow 'Nginx HTTP'
+apt-get install -y apt-utils autoconf automake build-essential git libcurl4-openssl-dev libgeoip-dev liblmdb-dev libpcre++-dev libtool libxml2-dev libyajl-dev pkgconf wget zlib1g-dev
+git clone --depth 1 -b v3/master --single-branch https://github.com/SpiderLabs/ModSecurity
+cd ModSecurity
+git submodule init
+git submodule update
+./build.sh
+./configure
+make
+make install
+cd ..
